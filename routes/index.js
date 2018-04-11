2629837c7d22c00c1389674fc81e1e745e9e3f14
Future Front-End Developer <grzegorz.stanczyk@o2.pl>
2018-04-11 22:51:25 +0200

    Added connection to mongodb

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,31 @@ module.exports = (app, dbs) => {
     res.send('Search images by typing words after sign /')
   })
   
-  app.get('/:id', (req, res) => {
+  app.get('/latest', (err, res) => {
+    dbs.collection('image-search-params')
+      .find({})
+      .sort({ _id: -1 })
+      .limit(10)
+      .toArray((err, result) => {
+        const val = result.map(v => {
+          return {
+            therm: v.params,
+            when: v._id.getTimestamp()
+          }
+        });
+        res.send(val);
+      })
+  })
+  
+  app.get('/search/:id', (req, res) => {
     const params = req.params.id;
+    if (params === 'favicon.ico' || params === 'robots.txt') {
+      return
+    } 
     const query = req.query.offset * 10 + 1 || 1;
     apiCall(params, query, (data) => {
       res.send(data)
+      dbs.collection('image-search-params').insert({params});
     })
   })
   
